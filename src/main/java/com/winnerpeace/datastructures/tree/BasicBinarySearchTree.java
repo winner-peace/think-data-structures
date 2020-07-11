6d7015bdef19a796e95c0ae059072a1ae3529a3c
Jaeyeonling <jaeyeonling@gmail.com>
2020-07-11 19:30:35 +0900

    BST insert, delete 구현

--- src/main/java/com/winnerpeace/datastructures/tree/BasicBinarySearchTree.java
+++ src/main/java/com/winnerpeace/datastructures/tree/BasicBinarySearchTree.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static java.util.Comparator.comparingInt;
-import static java.util.Objects.nonNull;
+import static java.util.Objects.isNull;
 
 @ToString
 @EqualsAndHashCode
@@ -20,8 +20,6 @@ public final class BasicBinarySearchTree<E> implements BinarySearchTree<E> {
     // Notice: 트리의 구조를 배우기 위해 해시 충돌 고려하지 않음
 
     private Node<E> root;
-
-    private int size;
 
     public static <E> BinarySearchTree<E> create(final List<E> values) {
         @SuppressWarnings("unchecked")
@@ -33,10 +31,7 @@ public final class BasicBinarySearchTree<E> implements BinarySearchTree<E> {
     public static <E> BinarySearchTree<E> create(final E[] values) {
         Arrays.sort(values, comparingInt(Object::hashCode));
 
-        final Node<E> root = createNode(values, 0, values.length - 1);
-        final int size = values.length;
-
-        return of(root, size);
+        return of(createNode(values, 0, values.length - 1));
     }
 
     @Nullable
@@ -57,12 +52,46 @@ public final class BasicBinarySearchTree<E> implements BinarySearchTree<E> {
 
     @Override
     public Optional<E> find(final int key) {
-        return findLocation(key).map(Node::getValue);
+        return find(root, key).map(Node::getValue);
+    }
+
+    private Optional<Node<E>> find(final Node<E> root,
+                             final int key) {
+        if (isNull(root) || root.matchKey(key)) {
+            return Optional.ofNullable(root);
+        }
+
+        if (root.graterThanBy(key)) {
+            return find(root.getLeft(), key);
+        }
+
+        return find(root.getRight(), key);
+    }
+
+    @Override
+    public void insert(final E value) {
+        root = insert(root, value);
+    }
+
+    private Node<E> insert(final Node<E> root,
+                           final E value) {
+        if (isNull(root)) {
+            return Node.root(value);
+        }
+
+        if (root.graterThanBy(value)) {
+            root.left(insert(root.getLeft(), value));
+        }
+        if (root.lessThanBy(value)) {
+            root.right(insert(root.getRight(), value));
+        }
+
+        return root;
     }
 
     @Override
     public Optional<E> delete(final int key) {
-        return null;
+        return delete(root, key).map(Node::getValue);
     }
 
     @Override
@@ -70,32 +99,49 @@ public final class BasicBinarySearchTree<E> implements BinarySearchTree<E> {
         return delete(value.hashCode()).isPresent();
     }
 
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    @Override
-    public void insert(final E value) {
-        if (isEmpty()) {
-            root = Node.root(value);
-            return;
+    private Optional<Node<E>> delete(final Node<E> root,
+                                     final int key) {
+        if (isNull(root)) {
+            return Optional.empty();
         }
 
-        final int key = value.hashCode();
+        if (root.graterThanBy(key)) {
+            root.setLeft(delete(root.getLeft(), key).orElse(null));
+        } else if (root.lessThanBy(key)) {
+            root.setRight(delete(root.getRight(), key).orElse(null));
+        } else {
+            if (root.isLeaf()) {
+                return Optional.empty();
+            }
+            if (root.emptyLeft()) {
+                return Optional.ofNullable(root.getRight());
+            }
+            if (root.emptyRight()) {
+                return Optional.ofNullable(root.getRight());
+            }
 
-        findLocation(key)
-                .filter(node -> node.nonMatchKey(key))
-                .map(node -> {
-                    ++size;
+            root.change(findMin(root.getRight()));
+            root.setRight(delete(root.getRight(), root.getKey()).orElse(null));
+        }
 
-                    if (node.graterThanBy(key)) {
-                        return node.left(value);
-                    }
+        return Optional.of(root);
+    }
 
-                    return node.right(value);
-                })
-                .orElseThrow(RuntimeException::new); // TODO Key is already insert exception
+    @SuppressWarnings("ConstantConditions")
+    @Nullable
+    private E findMin(final Node<E> root) {
+        if (isNull(root)) {
+            return null;
+        }
+
+        Node<E> node = root;
+        E min = node.getValue();
+        while (node.hasLeft()) {
+            min = node.getLeftValue();
+            node = node.getLeft();
+        }
+
+        return min;
     }
 
     @Override
@@ -105,32 +151,11 @@ public final class BasicBinarySearchTree<E> implements BinarySearchTree<E> {
 
     @Override
     public boolean exists(final int key) {
-        return findLocation(key).isPresent();
+        return find(key).isPresent();
     }
 
-    private Optional<Node<E>> findLocation(final int key) {
-        if (isEmpty()) {
-            return Optional.empty();
-        }
-
-        Node<E> parent = null;
-        Node<E> value = root;
-
-        while (nonNull(value)) {
-            if (value.matchKey(key)) {
-                return Optional.of(value);
-            }
-
-            parent = value;
-
-            if (value.graterThanBy(key)) {
-                value = value.getLeft();
-                continue;
-            }
-
-            value = value.getRight();
-        }
-
-        return Optional.ofNullable(parent);
+    @Override
+    public boolean isEmpty() {
+        return isNull(root);
     }
 }
--- src/main/java/com/winnerpeace/datastructures/tree/Node.java
+++ src/main/java/com/winnerpeace/datastructures/tree/Node.java
@@ -2,18 +2,17 @@ package com.winnerpeace.datastructures.tree;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 @ToString
 @EqualsAndHashCode
-@RequiredArgsConstructor(staticName = "root")
 public final class Node<E> {
 
     @Getter
@@ -27,15 +26,31 @@ public final class Node<E> {
     private Node<E> right;
 
     @Getter
-    private final E value;
+    private E value;
+
+    private Node(final E value) {
+        this.value = value;
+    }
+
+    public static <E> Node<E> root(final E value) {
+        return new Node<>(value);
+    }
 
     public Node<E> left(final E value) {
-        left = new Node<>(value);
+        return left(root(value));
+    }
+
+    public Node<E> left(final Node<E> node) {
+        left = node;
         return left;
     }
 
     public Node<E> right(final E value) {
-        right = new Node<>(value);
+        return right(root(value));
+    }
+
+    public Node<E> right(final Node<E> node) {
+        right = node;
         return right;
     }
 
@@ -59,11 +74,53 @@ public final class Node<E> {
         return !matchKey(key);
     }
 
+    public boolean graterThanBy(final E other) {
+        return graterThanBy(other.hashCode());
+    }
+
     public boolean graterThanBy(final int key) {
         return value.hashCode() > key;
     }
 
+    public boolean lessThanBy(final E other) {
+        return lessThanBy(other.hashCode());
+    }
+
     public boolean lessThanBy(final int key) {
         return value.hashCode() < key;
+    }
+
+    public boolean isLeaf() {
+        return emptyLeft() && emptyRight();
+    }
+
+    public boolean emptyLeft() {
+        return isNull(left);
+    }
+
+    public boolean hasLeft() {
+        return !emptyLeft();
+    }
+
+    public boolean emptyRight() {
+        return isNull(right);
+    }
+
+    public void change(final E value) {
+        this.value = value;
+    }
+
+    public int getKey() {
+        return value.hashCode();
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Nullable
+    public E getLeftValue() {
+        if (emptyLeft()) {
+            return null;
+        }
+
+        return left.getValue();
     }
 }
