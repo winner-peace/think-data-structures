ce2ba51fc89e53ea486707ef5d0fa24e5b9a788b
Jaeyeon Kim <jaeyeon@superbcorp.com>
2020-07-28 22:42:17 +0900

    QuickSort

--- src/test/java/com/winnerpeace/datastructures/jaeyeonling/sort/MiddleQuickSortTest.java
+++ src/test/java/com/winnerpeace/datastructures/jaeyeonling/sort/MiddleQuickSortTest.java
@@ -2,12 +2,12 @@ package com.winnerpeace.datastructures.jaeyeonling.sort;
 
 /**
  * 분할정복법 (Divide and Conquer)의 방법으로 전체 데이터를 기준 값을 중심으로 왼쪽 부분과 오른쪽 부분을 분할한 다음 정렬한다.
- * RightQuickSort는 맨 오른쪽을 기준으로 한다.
+ * RightQuickSort는 중앙을 기준으로 한다.
  * <p>
  * Pivot을 기준으로 여러가지 QuickSort가 존재한다.
  * LeftQuickSort, RightQuickSort, MiddleQuickSort, RandomQuickSort, DoubleQuickSort, ...
  */
-class RightQuickSortTest extends SortTest {
+class MiddleQuickSortTest extends SortTest {
 
     @Override
     int[] sort(final int[] values) {
@@ -19,30 +19,38 @@ class RightQuickSortTest extends SortTest {
     private void sort(final int[] values,
                       final int left,
                       final int right) {
-        if (left >= right) {
-            return;
-        }
-
         final int pivot = partition(values, left, right);
 
-        sort(values, left, pivot - 1);
-        sort(values, pivot + 1, right);
+        if (left < pivot - 1) {
+            sort(values, left, pivot - 1);
+        }
+        if (pivot < right) {
+            sort(values, pivot, right);
+        }
     }
 
     private int partition(final int[] values,
                           final int left,
                           final int right) {
-        final int pivot = values[right];
+        // Note: (left + right) / 2
+        final int middle = left + (right - left) / 2;
+        final int pivot = values[middle];
 
-        int newPivot = left;
-        for (int i = left; i < right; i++) {
-            if (values[i] <= pivot) {
-                swap(values, newPivot++, i);
+        int start = left;
+        int end = right;
+        while (start <= end) {
+            while (values[start] < pivot) {
+                ++start;
+            }
+            while (values[end] > pivot) {
+                --end;
+            }
+
+            if (start <= end) {
+                swap(values, start++, end--);
             }
         }
 
-        swap(values, newPivot, right);
-
-        return newPivot;
+        return start;
     }
 }
--- src/test/java/com/winnerpeace/datastructures/jaeyeonling/sort/RandomQuickSortTest.java
+++ src/test/java/com/winnerpeace/datastructures/jaeyeonling/sort/RandomQuickSortTest.java
@@ -1,13 +1,15 @@
 package com.winnerpeace.datastructures.jaeyeonling.sort;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * 분할정복법 (Divide and Conquer)의 방법으로 전체 데이터를 기준 값을 중심으로 왼쪽 부분과 오른쪽 부분을 분할한 다음 정렬한다.
- * RightQuickSort는 맨 오른쪽을 기준으로 한다.
+ * RandomQuickSort는 랜덤한 위치를 기준으로 한다.
  * <p>
  * Pivot을 기준으로 여러가지 QuickSort가 존재한다.
  * LeftQuickSort, RightQuickSort, MiddleQuickSort, RandomQuickSort, DoubleQuickSort, ...
  */
-class RightQuickSortTest extends SortTest {
+class RandomQuickSortTest extends SortTest {
 
     @Override
     int[] sort(final int[] values) {
@@ -19,30 +21,36 @@ class RightQuickSortTest extends SortTest {
     private void sort(final int[] values,
                       final int left,
                       final int right) {
-        if (left >= right) {
-            return;
-        }
-
         final int pivot = partition(values, left, right);
 
-        sort(values, left, pivot - 1);
-        sort(values, pivot + 1, right);
+        if (left < pivot - 1) {
+            sort(values, left, pivot - 1);
+        }
+        if (pivot < right) {
+            sort(values, pivot, right);
+        }
     }
 
     private int partition(final int[] values,
                           final int left,
                           final int right) {
-        final int pivot = values[right];
+        final int pivot = values[ThreadLocalRandom.current().nextInt(left, right)];
 
-        int newPivot = left;
-        for (int i = left; i < right; i++) {
-            if (values[i] <= pivot) {
-                swap(values, newPivot++, i);
+        int start = left;
+        int end = right;
+        while (start <= end) {
+            while (values[start] < pivot) {
+                ++start;
+            }
+            while (values[end] > pivot) {
+                --end;
+            }
+
+            if (start <= end) {
+                swap(values, start++, end--);
             }
         }
 
-        swap(values, newPivot, right);
-
-        return newPivot;
+        return start;
     }
 }
